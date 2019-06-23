7aa2f12281d54aa25d52f81478580bf11bd343e6
K1000ch <crowbarfreak@gmail.com>
2019-06-23 13:43:21 +0900

    qwer

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import logo from '../img/logo.svg'
+import facebook from '../img/social/facebook.svg'
+import instagram from '../img/social/instagram.svg'
+import twitter from '../img/social/twitter.svg'
+import vimeo from '../img/social/vimeo.svg'
+
+const footer = class extends React.Component {
+  render() {
+    return (
+        <footer>
+            ふったーです
+        </footer>
+    )
+  }
+}
+
+export default footer
---
+++ src/components/layout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+//import { Link } from 'gatsby'
+import Header from "./header"
+import Footer from "./footer"
+
+import "../css/reset.css"
+import "../css/globalSetting.css"
+import "../css/globalStyle.css"
+
+import layoutStyles from "../css/components/layout.module.css"
+
+
+
+export default ({ children }) => (
+  <div className={layoutStyles.wrapper}>
+      <Header className={layoutStyles.header}/>
+        <div className={layoutStyles.main}>
+          { children }
+        </div>
+      <Footer />
+  </div>
+)
