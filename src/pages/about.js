b4fae2f0ef073ace6621312b50530873ceed210c
K1000ch <crowbarfreak@gmail.com>
2019-06-23 13:16:27 +0900

    edit add

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 
 export default () => (
+  {/*}
   <Layout>
     <div>
       <h1>
@@ -12,10 +13,12 @@ export default () => (
         websites.
       </p>
     </div>
-    {/*
     <iframe width="560" height="415" src="https://www.youtube.com/embed/fR8OsFvkLpo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="560" height="415" src="https://www.youtube.com/embed/fR8OsFvkLpo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="560" height="415" src="https://www.youtube.com/embed/fR8OsFvkLpo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    */}
   </Layout>
+  */}
+  <div>
+    about
+  </div>
 )
