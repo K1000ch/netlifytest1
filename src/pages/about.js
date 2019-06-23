5d952ca86e69645089e5a7c7b099143b3086794d
K1000ch <crowbarfreak@gmail.com>
2019-06-23 13:20:26 +0900

    edit add

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,12 @@
 import React from "react"
-import Layout from "../components/layout"
+//import Layout from "../components/layout"
 
 export default () => (
-  {/*}
-  <Layout>
-    <div>
-      <h1>
-        about
-      </h1>
-      <p>
-        What do I like to do? Lots of course but definitely enjoy building
-        websites.
-      </p>
-    </div>
-    <iframe width="560" height="415" src="https://www.youtube.com/embed/fR8OsFvkLpo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <iframe width="560" height="415" src="https://www.youtube.com/embed/fR8OsFvkLpo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <iframe width="560" height="415" src="https://www.youtube.com/embed/fR8OsFvkLpo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </Layout>
-  */}
   <div>
-    about
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
   </div>
 )
