63297c4305b25ec5444593174c5bba95ce80a89f
K1000ch <crowbarfreak@gmail.com>
2019-06-24 13:56:56 +0900

    edit header and somepages for add query

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import GloalNav from "./globalNavigation"
+import { StaticQuery, graphql } from "gatsby"
+
 
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
@@ -8,7 +10,8 @@ import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
 
-const header = class extends React.Component {
+/*
+const header = class extends React.Component ()
   render() {
     return (
         <header>
@@ -22,3 +25,33 @@ const header = class extends React.Component {
 }
 
 export default header
+*/
+
+
+
+export default () => (
+  <StaticQuery
+    query={
+      graphql`
+        {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }      
+      `
+    }
+    render = {
+      data => (
+        <header>
+          <h1>
+            {data.site.siteMetadata.title}
+          </h1>
+          <GloalNav />
+        </header>
+      )
+    }
+
+  />
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,38 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link } from 'gatsby'
+import { graphql } from "gatsby"
 
-export default () => (
+
+export default ({data}) => (
+//  console.log(data)
+
   <Layout>
     <div>
       <h1>
         About
       </h1>
       <p>
-
+        {data.allMarkdownRemark.edges[0].node.excerpt}
       </p>
     </div>
   </Layout>
+
 )
+
+export const query = graphql`
+{
+  allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "about-page"}}}) {
+    edges {
+      node {
+        html
+        frontmatter {
+          title
+          templateKey
+        }
+        excerpt(format: PLAIN)
+      }
+    }
+  }
+}
+`
