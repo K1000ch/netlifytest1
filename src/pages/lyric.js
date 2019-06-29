56cadcc49863e2a04fb2a36fff5d3f732e77bfa5
K1000ch <crowbarfreak@gmail.com>
2019-06-29 13:40:06 +0900

    lyricページを追加

---
+++ src/pages/lyric.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Link } from 'gatsby'
+
+export default () => (
+  <Layout>
+    <div>
+      <h1>
+        Lyrics
+      </h1>
+      <p>
+
+      </p>
+    </div>
+  </Layout>
+)
