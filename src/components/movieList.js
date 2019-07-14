5b590df9b006e401d5bed2da73e4dbcca1236368
K1000ch <crowbarfreak@gmail.com>
2019-07-14 21:49:40 +0900

    youtube埋め込みを修正、musicページのレイアウト修正

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -21,6 +21,7 @@ export default () => {
     }    
     `
   )
+  /*
   return (
     <ul className="movieList">
       {data.allMarkdownRemark.edges.map(
@@ -50,5 +51,68 @@ export default () => {
           </li>
       ))} 
     </ul>
+  )
+  */
+
+ return (
+  <ul className="movieList">
+    <li>
+      <div className="date">
+        <h3>
+          Radiohead - Just
+        </h3>
+      </div>
+      <div className="article">
+        <div className="movieWraper">
+          <div className="iframeWrap">
+            <iframe
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/oIFLtNYI3Ls"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen>
+
+            </iframe>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div className="date">
+        <h3>
+          TTNG - Adventure, Stamina & Anger - Audiotree Live
+        </h3>
+      </div>
+      <div className="article">
+        <div className="movieWraper">
+          <div className="iframeWrap">
+            <iframe 
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/E6t3_XwDvkg"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+            >
+            </iframe>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div className="date">
+        <h3>
+          Joy Division - Transmission [OFFICIAL MUSIC VIDEO]
+        </h3>
+      </div>
+      <div className="article">
+        <div className="movieWraper">
+          <div className="iframeWrap">
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/6dBt3mJtgJc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
   )
 }
